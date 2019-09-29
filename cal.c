6b9dc147595bfa37c139c9ca296c06f3f18c4dc1
kranthi127 <55928459+kranthi127@users.noreply.github.com>
2019-09-29 06:38:28 -0500

    addfunadd.c

--- cal.c
+++ cal.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
+int add(int,int);
 int main()
 {
 int a,b;
 printf("enter a,b values");
 scanf("%d%d",&a,&b);
 printf("%d",add(a,b));
+}
+int add(int a,int b)
+{
+ return(a+b);
 }
