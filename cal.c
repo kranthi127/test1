9bdfa931a9fa1314ca2e95877dd9de1b55da76d5
kranthi127 <kranthi127eee@gmail.com>
2019-09-30 18:32:28 +0530

    changes

--- cal.c
+++ cal.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
 int add(int,int);
 int mul(int,int);
+int div(int,int);
 int main()
 {
 int a,b;
 printf("enter a,b values");
 scanf("%d%d",&a,&b);
 printf("%d",add(a,b));
- printf("/n%d",mul(a,b);
+ printf("\n%d",mul(a,b));
+printf("\n%d",div(a,b));
 }
 int add(int a,int b)
 {
@@ -17,3 +19,7 @@ int add(int a,int b)
         {
         return(a*b);
         }
+int div(int a,int b)
+{
+return (a/b);
+}
